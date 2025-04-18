d9ff02a30006181ba242e5891d8e0656c71cbe79
Kalinin Dmitry <didiwot@yandex.ru>
2025-04-18 15:35:51 +0300

    надо сделать сайт вове

---
+++ siteVova/site_for_vladivir/src/main/java/twentyoneh/site_for_vladivir/SiteForVladivirApplication.java
@@ -0,0 +1,13 @@
+package twentyoneh.site_for_vladivir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteForVladivirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteForVladivirApplication.class, args);
+	}
+
+}
---
+++ siteVova/site_for_vladivir/src/test/java/twentyoneh/site_for_vladivir/SiteForVladivirApplicationTests.java
@@ -0,0 +1,13 @@
+package twentyoneh.site_for_vladivir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteForVladivirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
